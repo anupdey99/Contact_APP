9d61b1860d95c314376771927db1cdd1c9293cbe
anupdey99 <anupdey99@gmail.com>
2016-03-13 22:54:48 +0600

    A simple Contact App using SQLite Database
    Contact App that can store contact data, modify them or delete them.
    Call & SMS function enabled.

---
+++ app/src/main/java/com/adlabsbd/anup/contact/ContactModel.java
@@ -0,0 +1,47 @@
+package com.adlabsbd.anup.contact;
+
+/**
+ * Created by Anup on 13-Mar-16.
+ */
+public class ContactModel {
+
+    private String name;
+    private String phoneNO;
+    private int id;
+    //private int imageID;
+
+    public ContactModel(String name, String phoneNO, int id) {
+        this.name = name;
+        this.phoneNO = phoneNO;
+        this.id = id;
+    }
+
+    public ContactModel(String name, String phoneNO) {
+        this.name = name;
+        this.phoneNO = phoneNO;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNO() {
+        return phoneNO;
+    }
+
+    public void setPhoneNO(String phoneNO) {
+        this.phoneNO = phoneNO;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+}
